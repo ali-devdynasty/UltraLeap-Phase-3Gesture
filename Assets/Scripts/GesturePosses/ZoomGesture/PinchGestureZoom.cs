58c7e25eb6d36102052e13344553a1e0f74d4fd0
Mukkarams1 <mukkarams1@gmail.com>
2024-08-12 02:03:39 +0500

    PinchFix

--- Assets/Scripts/GesturePosses/ZoomGesture/PinchGestureZoom.cs
+++ Assets/Scripts/GesturePosses/ZoomGesture/PinchGestureZoom.cs
@@ -60,5 +60,9 @@ public class PinchZoomGesture : MonoBehaviour
 
             }
         }
+        if (groupController == null)
+        {
+            groupController = FindObjectOfType<GroupController>();
+        }
     }
 }
